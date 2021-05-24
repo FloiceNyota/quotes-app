a781a0047ec3245dc23419d4c2819232ee016e3a
FloiceNyota98 <nyotafloice@gmail.com>
2021-05-24 22:10:10 +0300

    do quote.component.html

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  title: string = 'quote-app';
   constructor() { }
 
   ngOnInit(): void {
